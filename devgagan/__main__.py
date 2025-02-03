c0c6757ef9eedaa93324259a837d50a575dce192
adarshmaurya541 <adarshgdsc2024@gmail.com>
2025-02-03 19:28:28 +0530

    cc

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -40,10 +40,8 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+👨‍💻 Author: Adarsh
+📬 Telegram: https://t.me/Contact_xbot
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
