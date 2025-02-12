ab1e04c0fdb82a3825e39bbf2e688d509f640d25
adarshmaurya541 <adarshgdsc2024@gmail.com>
2025-02-12 10:45:44 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -26,7 +26,7 @@ from devgagan.core.mongo import db
 from pyrogram.errors import FloodWait
 from datetime import datetime, timedelta
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from devgagan.core.mongo import user_sessions_real
+from devgagan.core.mongo.bd import user_sessions_real
 import subprocess
 from devgagan.modules.shrink import is_user_verified
 async def generate_random_name(length=8):
