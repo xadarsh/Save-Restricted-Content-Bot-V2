04abeafcea0eb8d19e952a964458a0d53608b08f
adarshmaurya541 <adarshgdsc2024@gmail.com>
2025-02-14 21:20:56 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -390,3 +390,4 @@ async def show_admin_commands(client, message):
 app = Client("chat_bot", api_id=API_ID, api_hash=API_HASH)
 # Call register_handlers to add all necessary handlers
 register_handlers(app)
+app.run()
